e5c2d4ea9ca0399afc358b457a4af8662a3fa510
George Bennett <george6bennett@gmail.com>
2020-06-22 21:56:27 +0100

    add test beginnings and fixtures

---
+++ README.md
@@ -0,0 +1,15 @@
+## Setup
+
+To run the scripts locally you'll need to install the [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html) and configure your credentials. The `botocore` AWS client will read your credentials from the environment.
+
+## Install
+
+```
+pip3 install -r requirements.txt
+```
+
+## Package
+
+```
+make all
+```
--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 """
@@ -5,3 +6,10 @@ The src directory is included in PATH when the Python interpreter is invoked by 
 However it's not included when the interpreter is invoked by PyTest, so we append it manually
 """
 sys.path.append(f"{sys.path[0]}/src")
+
+"""
+Mock AWS credentials during tests
+"""
+os.environ["AWS_ACCESS_KEY_ID"] = "MOCK_ID"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "MOCK_KEY"
+os.environ["AWS_DEFAULT_REGION"] = "eu-west-1"
