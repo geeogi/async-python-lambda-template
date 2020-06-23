8d788f98d5b941f917405de9caf3cdf18f8aed18
George Bennett <george6bennett@gmail.com>
2020-06-23 09:21:59 +0100

    add aioresponses for mocking http

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,46 +1,44 @@
+import json
 from unittest.mock import patch
 
 import boto3
 import responses
+from aioresponses import aioresponses
 from moto import mock_s3
-import json
 
 from main import handler
 
 
-@responses.activate
+@aioresponses()
 @patch("main.aiobotocore")
-def test_handler(mock_aiobotocore):
+def test_handler(*mocks):
+    mock_aioresponses, mock_aiobotocore = mocks
+    
     """
     Mock responses for news endpoints 
     """
-    responses.add(
-        method=responses.GET,
-        url="https://www.bbc.co.uk/news",
+    mock_aioresponses.get(
+        "https://www.bbc.co.uk/news",
         body=open("./tests/fixtures/news/bbc.txt").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://www.wsj.com/",
+    mock_aioresponses.get(
+        "https://www.wsj.com/",
         body=open("./tests/fixtures/news/wsj.txt").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://www.ft.com/",
+    mock_aioresponses.get(
+        "https://www.ft.com/",
         body=open("./tests/fixtures/news/ft.txt").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://uk.finance.yahoo.com/",
+    mock_aioresponses.get(
+        "https://uk.finance.yahoo.com/",
         body=open("./tests/fixtures/news/yahoo.txt").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://www.coindesk.com/",
+    mock_aioresponses.get(
+        "https://www.coindesk.com/",
         body=open("./tests/fixtures/news/coindesk.txt").read(),
         status=200
     )
@@ -48,21 +46,18 @@ def test_handler(mock_aiobotocore):
     """
     Mock responses for price endpoints 
     """
-    responses.add(
-        method=responses.GET,
-        url="https://api.coingecko.com/api/v3/exchange_rates",
+    mock_aioresponses.get(
+        "https://api.coingecko.com/api/v3/exchange_rates",
         body=open("./tests/fixtures/prices/coingecko.json").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://api.gemini.com/v1/pubticker/btcusd",
+    mock_aioresponses.get(
+        "https://api.gemini.com/v1/pubticker/btcusd",
         body=open("./tests/fixtures/prices/gemini.json").read(),
         status=200
     ),
-    responses.add(
-        method=responses.GET,
-        url="https://api.blockchain.info/stats",
+    mock_aioresponses.get(
+        "https://api.blockchain.info/stats",
         body=open("./tests/fixtures/prices/blockchain.json").read(),
         status=200
     )
@@ -72,7 +67,12 @@ def test_handler(mock_aiobotocore):
     """
     handler(None, None)
 
+    """
+    Validate 
+    """
     mock_session = mock_aiobotocore.get_session.return_value
     mock_context_manager = mock_session.create_client.return_value
     mock_client = mock_context_manager.__aenter__.return_value
     mock_put_object = mock_client.put_object
+
+    assert len(mock_put_object.mock_calls) == 2
